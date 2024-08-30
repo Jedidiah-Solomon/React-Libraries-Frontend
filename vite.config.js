e8f182d3d70478bf35667c3f3ab24058b840b30e
Jedidiah Solomon Onwubiko <121108148+Jedidiah-Solomon@users.noreply.github.com>
2024-08-30 16:08:10 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,17 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: import.meta.env.VITE_PORT,
+    port: import.meta.env.VITE_PORT
+      ? parseInt(import.meta.env.VITE_PORT)
+      : 5000,
     open: true,
+    proxy: {
+      "/api": {
+        target: import.meta.env.VITE_BACKEND_URL || "http://localhost:8000",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
 });
